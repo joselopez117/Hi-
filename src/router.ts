175582af1b7fd1d273c0501d7937e2eee3a55186
joselopez117 <74109741+joselopez117@users.noreply.github.com>
2024-11-17 00:24:01 -0600

    feat: using web hashing test

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Hi from './components/Hi.vue';
 import ContactPage from './components/ContactPage.vue';
 
@@ -8,7 +8,7 @@ const routes = [
   ];
 
 const router = createRouter({
-    history: createWebHistory(), // Uses browser history mode
+    history: createWebHashHistory(), // Uses browser history mode
     routes,
 });
 
