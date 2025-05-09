80ca89558584723bdf2167a4ad9505dc623e49ad
joselopez117 <74109741+joselopez117@users.noreply.github.com>
2025-05-09 12:03:30 -0500

    feat: updated home page

--- src/components/Hi.vue
+++ src/components/Hi.vue
@@ -6,14 +6,21 @@ defineProps<{
   msg: string
  }>()
 
-const hiMsg = ref('Hi!')
+const hiMsg = ref("Hello hello! I'm Jose Lopez")
+const aspirationalRoles = ref("Software Engineer • Machine learning enthusiast")
+const aboutMe = ref('About Me');
 const welcomeMsg = ref("Welcome to my (in progress) professional showcase! I'm a passionate software developer with expertise in crafting innovative solutions across the stack. With experience in frameworks like Vue.js, Knockout.js, and Next.js, coupled with strong back-end skills in C#, Java, and Node.js, I thrive in building seamless, efficient, and user-focused applications.");
+const projects = ref('Projects');
 </script>
 
 <template>
   <div class="parent-text">
     <h1>{{ hiMsg }}</h1>
+    <h2>{{ aspirationalRoles }}</h2>
+    <h2>{{ aboutMe }}</h2>
     <div>{{ welcomeMsg }}</div>
+    <h2>{{ projects }}</h2>
+    
     <FidgetButton />
   </div>
 </template>
