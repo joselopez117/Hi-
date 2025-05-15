07262ec27771b9e04607bdcf14038b5c3fec6535
joselopez117 <74109741+joselopez117@users.noreply.github.com>
2025-05-15 12:05:36 -0500

    feat: update to contact me page

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -3,25 +3,10 @@ import axios from 'axios';
 import FidgetButton from './FidgetButton.vue';
 import { ref } from 'vue';
 
-interface ResponseObject {
-    Name : string,
-    // email : string, //TODO: to be added back in at some point
-    message : string,
-    serverMessage: string,
-    id: number, //TODO: id should be generated in backend
-}
-
 const name = ref('');
-// const email = ref('');
 const message = ref('');
 const count = ref(0);
-let responseBody: ResponseObject = {
-    Name: '',
-    // email: '',
-    message: '',
-    serverMessage: '',
-    id: 0,//TODO: id should be generated in backend
-};
+const responseReturned = ref('');
 
 function deliverVistorInfo() {  
     if(name.value != '' && message.value != ''){
@@ -38,41 +23,27 @@ function deliverVistorInfo() {
                 body,
             ) 
             .then(response => {
-                responseBody.Name = response.data.Name;
-                //responseBody.email = response.data.email; //TODO: add this back in at some point 
-                responseBody.message = response.data.Message;
-                responseBody.id = response.data.Id;
-                //responseBody.serverMessage = response.data.ServerMessage;
+                if(response){
+                    responseReturned.value = "Message sent!" 
+                }
             }).catch(response => {
-                console.log(response);
-            }).finally(() => {
-                name.value = '';
-                // email.value = '';
-                message.value = '';//TODO: finally may not be needed
-            });
+                // console.log(response);
+            })
     }
 }
-
 </script>
 
 <template>
     <div class="contact-form">
         <form class="form-data" @submit.prevent>
-            <label for="name">First name:</label>
+            <label for="name">Name:</label>
             <input type="text" id="name" name="name" v-model="name"><br>
-            <!-- TODO: add message box here for users to send a message to me -->
             <label for="message">Message:</label>
             <textarea id="message" name="message" v-model="message" class="msg-area"></textarea><br>
             <input type="submit" value="Submit" @click="deliverVistorInfo">
         </form>
     </div>
-    <div>
-        {{ responseBody }}
-    </div>
     <div class="text-parent">
-        <div>Pardon the dust!</div>
-        <div>I am working on a contact form. In the mean time here are my socials:</div>
-        <br/>
         <a href="https://github.com/joselopez117">Github</a>
         <br/>
         <span>You can also click the picture on the top right!</span>
